1633711b58d0a20191e696f875852f07d44c43ee
EvgenyS <admin@example.com>
2019-04-17 19:16:19 +0300

    Item controller + page templates
    добавил шаблоны страниц и сам контроллер

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'items#index'
+
+  get 'imports', to: 'items#import'
+
+  resources :items, only: [:index, :show] do
+    collection do
+      get 'import'
+    end
+  end  
 end
